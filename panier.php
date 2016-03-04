38d72d224be3e3e0248f2794c298ab65ebf3b266
LOMBARD Alan <LOMBARD Alan@CP-SIO-W10-PG14.sio.lan>
2016-03-04 09:46:38 +0100

    Panier: remplacement  des mysql_query et _num_rows par des mysqli

--- panier.php
+++ panier.php
@@ -6,8 +6,8 @@ if (!isset($_SESSION["id"])) { ?>
 <div id="divExplication">
   <div class="petitTitre">PANIER</div><br />
   <div class="petitTexte">
-    Attention le panier n'est accessible que si vous êtes reconnu.<br />
-    Identifiez-vous sur la page accueil, ou créez un nouveau compte sur la page<br />
+    Attention le panier n'est accessible que si vous ï¿½tes reconnu.<br />
+    Identifiez-vous sur la page accueil, ou crï¿½ez un nouveau compte sur la page<br />
     votre espace perso.
   </div>
 </div>
@@ -19,15 +19,15 @@ if (!isset($_SESSION["id"])) { ?>
   <div class="petitTitre">PANIER</div><br />
   <div class="petitTexte">
     Votre panier contient les articles et le t-shirt que vous avez choisis :<br />
-    vous pouvez supprimer des éléments de votre sélection.<br />
-    Pensez à enregistrer votre panier pour passer au paiement.
+    vous pouvez supprimer des ï¿½lï¿½ments de votre sï¿½lection.<br />
+    Pensez ï¿½ enregistrer votre panier pour passer au paiement.
   </div>
 </div>
 
 <div id="divArticles" class="petitTexte">
   <?php           
   
-    //--- création du tableau pour afficher les articles ---
+    //--- crï¿½ation du tableau pour afficher les articles ---
     $k = 0 ;        // indice sur tous les articles
     $i = 0 ;        // indice sur les articles du panier      
     $total = 0 ;    // calcul du total de la facture
@@ -42,26 +42,27 @@ if (!isset($_SESSION["id"])) { ?>
       // si le fichier est une image
       if ($fic!="." && $fic!=".." && exif_imagetype ($ficComplet)) {
       
-        //--- récupération de l'article que s'il est dans le panier ---
-        $curseur = mysql_query("select * from panier where idclient=".$id." and idarticle=".$k) ;
-        if (mysql_num_rows($curseur)!=0) {
+        //--- rï¿½cupï¿½ration de l'article que s'il est dans le panier ---
+          $link = mysqli_connect("localhost", "root", "", "boutique");
+        $curseur = mysqli_query($link,"select * from panier where idclient=".$id." and idarticle=".$k) ;
+        if (mysqli_num_rows($curseur)!=0) {
           $nom = substr($fic,0,strlen($fic)-4) ;
-          // récupération des informations du fichier image
+          // rï¿½cupï¿½ration des informations du fichier image
           $infos = exif_read_data($ficComplet, 0, true) ;
           $titre = enleveUnSurDeux($infos["IFD0"]["Title"]) ;
           $sujet = enleveUnSurDeux($infos["IFD0"]["Subject"]) ;
           $commentaire = enleveUnSurDeux($infos["IFD0"]["Comments"]) ;
-          // construction de la ligne à gauche   
+          // construction de la ligne ï¿½ gauche   
           $lesarticles .= '<tbody id="ppanier'.$i.'"><tr id="trpanier'.$i.'">' ;    
           $lesarticles .= '<td class="td1"><img id="img'.$i.'" src="'.$ficComplet.'" alt="'.$k.'" class="imgBoutique" /></td>' ; 
           $lesarticles .= '<td class="td2">'.$titre.'<br />'.$commentaire.'</td>' ;
-          $lesarticles .= '<td class="td3"><label>'.$sujet.'</label>€</td>' ;
+          $lesarticles .= '<td class="td3"><label>'.$sujet.'</label>ï¿½</td>' ;
           $lesarticles .= '<td class="td4"><img id="suppr'.$i.'" src="images/corbeille.jpg" alt="supprimer" /></td>' ;
           $lesarticles .= '</tr></tbody>' ;
           // construction de la ligne dans le ticket
           $leticket .= '<tbody id="b'.$i.'"><tr id="trticket'.$i.'">' ;    
           $leticket .= '<td class="td2">'.$titre.'</td>' ;
-          $leticket .= '<td class="td3"><label id="prix'.$i.'">'.$sujet.'</label>€</td>' ;
+          $leticket .= '<td class="td3"><label id="prix'.$i.'">'.$sujet.'</label>ï¿½</td>' ;
           $leticket .= '</tr></tbody>' ;
           $total += $sujet ;
           
@@ -76,21 +77,21 @@ if (!isset($_SESSION["id"])) { ?>
     if (isset($_SESSION["couleur"])) {
       $nom = $_SESSION["couleur"] ;
       $ficComplet = "images/tshirts/".$nom.".jpg" ;
-      // récupération des informations du tshirt
+      // rï¿½cupï¿½ration des informations du tshirt
       $titre = "t-shirt ".$nom ;
       $sujet = $prixTshirt ;
-      $commentaire = "vous avez choisi un tshirt personnalisé." ;    
-      // construction de la ligne à gauche   
+      $commentaire = "vous avez choisi un tshirt personnalisï¿½." ;    
+      // construction de la ligne ï¿½ gauche   
       $lesarticles .= '<tbody id="ppanier'.$i.'"><tr id="trpanier'.$i.'">' ;    
       $lesarticles .= '<td class="td1"><img id="imgtshirt" src="'.$ficComplet.'" alt="'.$k.'" class="imgBoutique" /></td>' ; 
       $lesarticles .= '<td class="td2">'.$titre.'<br />'.$commentaire.'</td>' ;
-      $lesarticles .= '<td class="td3"><label>'.$sujet.'</label>€</td>' ;
+      $lesarticles .= '<td class="td3"><label>'.$sujet.'</label>ï¿½</td>' ;
       $lesarticles .= '<td class="td4"><img id="supprtshirt'.$i.'" src="images/corbeille.jpg" alt="supprimer" /></td>' ;
       $lesarticles .= '</tr></tbody>' ;
       // construction de la ligne dans le ticket
       $leticket .= '<tbody id="b'.$i.'"><tr id="trticket'.$i.'">' ;    
       $leticket .= '<td class="td2">'.$titre.'</td>' ;
-      $leticket .= '<td class="td3"><label id="prix'.$i.'">'.$sujet.'</label>€</td>' ;
+      $leticket .= '<td class="td3"><label id="prix'.$i.'">'.$sujet.'</label>ï¿½</td>' ;
       $leticket .= '</tr></tbody>' ;      
       $total += $prixTshirt ;
     }
@@ -98,7 +99,7 @@ if (!isset($_SESSION["id"])) { ?>
     // calcul du total du ticket
     $leticket .= '<tbody><tr id="trticket'.$i.'">' ;    
     $leticket .= '<td class="td2"><strong>TOTAL :</strong></td>' ;
-    $leticket .= '<td class="td3"><strong><label id="totalTicket">'.$total.'</label>€</strong></td>' ;
+    $leticket .= '<td class="td3"><strong><label id="totalTicket">'.$total.'</label>ï¿½</strong></td>' ;
     $leticket .= '</tr></tbody>' ;
     $leticket .= "</table>" ;
     
