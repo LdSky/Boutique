e9e668632a1e459edebcc93a34db75e23ea8efa1
LOMBARD Alan <LOMBARD Alan@CP-SIO-W10-PG14.sio.lan>
2016-03-04 10:55:30 +0100

    Panier/ perso passage en dbo

--- panier.php
+++ panier.php
@@ -1,124 +1,134 @@
-<?php include ("head.php") ;
+<?php
+include ("head.php");
 
 //--- panier non accessible si la personne n'est pas reconnue ---
-if (!isset($_SESSION["id"])) { ?>
+if (!isset($_SESSION["id"])) {
+    ?>
 
-<div id="divExplication">
-  <div class="petitTitre">PANIER</div><br />
-  <div class="petitTexte">
-    Attention le panier n'est accessible que si vous �tes reconnu.<br />
-    Identifiez-vous sur la page accueil, ou cr�ez un nouveau compte sur la page<br />
-    votre espace perso.
-  </div>
-</div>
+    <div id="divExplication">
+        <div class="petitTitre">PANIER</div><br />
+        <div class="petitTexte">
+            Attention le panier n'est accessible que si vous �tes reconnu.<br />
+            Identifiez-vous sur la page accueil, ou cr�ez un nouveau compte sur la page<br />
+            votre espace perso.
+        </div>
+    </div>
 
-<?php 
-}else{ ?>
+    <?php } else {
+    ?>
 
-<div id="divExplication">
-  <div class="petitTitre">PANIER</div><br />
-  <div class="petitTexte">
-    Votre panier contient les articles et le t-shirt que vous avez choisis :<br />
-    vous pouvez supprimer des �l�ments de votre s�lection.<br />
-    Pensez � enregistrer votre panier pour passer au paiement.
-  </div>
-</div>
+    <div id="divExplication">
+        <div class="petitTitre">PANIER</div><br />
+        <div class="petitTexte">
+            Votre panier contient les articles et le t-shirt que vous avez choisis :<br />
+            vous pouvez supprimer des �l�ments de votre s�lection.<br />
+            Pensez � enregistrer votre panier pour passer au paiement.
+        </div>
+    </div>
 
-<div id="divArticles" class="petitTexte">
-  <?php           
-  
-    //--- cr�ation du tableau pour afficher les articles ---
-    $k = 0 ;        // indice sur tous les articles
-    $i = 0 ;        // indice sur les articles du panier      
-    $total = 0 ;    // calcul du total de la facture
-    $lesarticles = '<table id="tPanier">' ;
-    $leticket = '<table id="tSelection">' ;
-    $chemin = "images/articles/" ;
-    $flux = dir($chemin) ;
+    <div id="divArticles" class="petitTexte">
+        <?php
+        //--- cr�ation du tableau pour afficher les articles ---
+        $k = 0;        // indice sur tous les articles
+        $i = 0;        // indice sur les articles du panier      
+        $total = 0;    // calcul du total de la facture
+        $lesarticles = '<table id="tPanier">';
+        $leticket = '<table id="tSelection">';
+        $chemin = "images/articles/";
+        $flux = dir($chemin);
 
-    //--- boucle sur tous les articles du dossier ---
-    while ($fic = $flux->read()) {
-      $ficComplet = $chemin.$fic ;
-      // si le fichier est une image
-      if ($fic!="." && $fic!=".." && exif_imagetype ($ficComplet)) {
-      
-        //--- r�cup�ration de l'article que s'il est dans le panier ---
-          $link = mysqli_connect("localhost", "root", "", "boutique");
-        $curseur = mysqli_query($link,"select * from panier where idclient=".$id." and idarticle=".$k) ;
-        if (mysqli_num_rows($curseur)!=0) {
-          $nom = substr($fic,0,strlen($fic)-4) ;
-          // r�cup�ration des informations du fichier image
-          $infos = exif_read_data($ficComplet, 0, true) ;
-          $titre = enleveUnSurDeux($infos["IFD0"]["Title"]) ;
-          $sujet = enleveUnSurDeux($infos["IFD0"]["Subject"]) ;
-          $commentaire = enleveUnSurDeux($infos["IFD0"]["Comments"]) ;
-          // construction de la ligne � gauche   
-          $lesarticles .= '<tbody id="ppanier'.$i.'"><tr id="trpanier'.$i.'">' ;    
-          $lesarticles .= '<td class="td1"><img id="img'.$i.'" src="'.$ficComplet.'" alt="'.$k.'" class="imgBoutique" /></td>' ; 
-          $lesarticles .= '<td class="td2">'.$titre.'<br />'.$commentaire.'</td>' ;
-          $lesarticles .= '<td class="td3"><label>'.$sujet.'</label>�</td>' ;
-          $lesarticles .= '<td class="td4"><img id="suppr'.$i.'" src="images/corbeille.jpg" alt="supprimer" /></td>' ;
-          $lesarticles .= '</tr></tbody>' ;
-          // construction de la ligne dans le ticket
-          $leticket .= '<tbody id="b'.$i.'"><tr id="trticket'.$i.'">' ;    
-          $leticket .= '<td class="td2">'.$titre.'</td>' ;
-          $leticket .= '<td class="td3"><label id="prix'.$i.'">'.$sujet.'</label>�</td>' ;
-          $leticket .= '</tr></tbody>' ;
-          $total += $sujet ;
-          
-          $i++ ;
+        //--- boucle sur tous les articles du dossier ---
+        while ($fic = $flux->read()) {
+            $ficComplet = $chemin . $fic;
+            // si le fichier est une image
+            if ($fic != "." && $fic != ".." && exif_imagetype($ficComplet)) {
+
+                //--- r�cup�ration de l'article que s'il est dans le panier ---
+
+                try {
+                    $db = new PDO("mysql:host=localhost;dbname=boutique;charset=utf8", "root", "");
+                    $mysecuredata = 14;
+                    $curseur = $db->query("select * from panier where idclient=" . $id . " and idarticle=" . $k);
+                } catch (PDOException $e) {
+                    echo "Error: " . $e;
+                }
+
+               
+                if ($curseur->rowCount() != 0) {
+                    $nom = substr($fic, 0, strlen($fic) - 4);
+                    // r�cup�ration des informations du fichier image
+                    $infos = exif_read_data($ficComplet, 0, true);
+                    $titre = enleveUnSurDeux($infos["IFD0"]["Title"]);
+                    $sujet = enleveUnSurDeux($infos["IFD0"]["Subject"]);
+                    $commentaire = enleveUnSurDeux($infos["IFD0"]["Comments"]);
+                    // construction de la ligne � gauche   
+                    $lesarticles .= '<tbody id="ppanier' . $i . '"><tr id="trpanier' . $i . '">';
+                    $lesarticles .= '<td class="td1"><img id="img' . $i . '" src="' . $ficComplet . '" alt="' . $k . '" class="imgBoutique" /></td>';
+                    $lesarticles .= '<td class="td2">' . $titre . '<br />' . $commentaire . '</td>';
+                    $lesarticles .= '<td class="td3"><label>' . $sujet . '</label>�</td>';
+                    $lesarticles .= '<td class="td4"><img id="suppr' . $i . '" src="images/corbeille.jpg" alt="supprimer" /></td>';
+                    $lesarticles .= '</tr></tbody>';
+                    // construction de la ligne dans le ticket
+                    $leticket .= '<tbody id="b' . $i . '"><tr id="trticket' . $i . '">';
+                    $leticket .= '<td class="td2">' . $titre . '</td>';
+                    $leticket .= '<td class="td3"><label id="prix' . $i . '">' . $sujet . '</label>�</td>';
+                    $leticket .= '</tr></tbody>';
+                    $total += $sujet;
+
+                    $i++;
+                }
+                $k++;
+            }
         }
-        $k++ ;
-      } 
-    }
-    $flux->close() ;
-    
-    // ajout d'une ligne s'il y a un tshirt 
-    if (isset($_SESSION["couleur"])) {
-      $nom = $_SESSION["couleur"] ;
-      $ficComplet = "images/tshirts/".$nom.".jpg" ;
-      // r�cup�ration des informations du tshirt
-      $titre = "t-shirt ".$nom ;
-      $sujet = $prixTshirt ;
-      $commentaire = "vous avez choisi un tshirt personnalis�." ;    
-      // construction de la ligne � gauche   
-      $lesarticles .= '<tbody id="ppanier'.$i.'"><tr id="trpanier'.$i.'">' ;    
-      $lesarticles .= '<td class="td1"><img id="imgtshirt" src="'.$ficComplet.'" alt="'.$k.'" class="imgBoutique" /></td>' ; 
-      $lesarticles .= '<td class="td2">'.$titre.'<br />'.$commentaire.'</td>' ;
-      $lesarticles .= '<td class="td3"><label>'.$sujet.'</label>�</td>' ;
-      $lesarticles .= '<td class="td4"><img id="supprtshirt'.$i.'" src="images/corbeille.jpg" alt="supprimer" /></td>' ;
-      $lesarticles .= '</tr></tbody>' ;
-      // construction de la ligne dans le ticket
-      $leticket .= '<tbody id="b'.$i.'"><tr id="trticket'.$i.'">' ;    
-      $leticket .= '<td class="td2">'.$titre.'</td>' ;
-      $leticket .= '<td class="td3"><label id="prix'.$i.'">'.$sujet.'</label>�</td>' ;
-      $leticket .= '</tr></tbody>' ;      
-      $total += $prixTshirt ;
-    }
-    
-    // calcul du total du ticket
-    $leticket .= '<tbody><tr id="trticket'.$i.'">' ;    
-    $leticket .= '<td class="td2"><strong>TOTAL :</strong></td>' ;
-    $leticket .= '<td class="td3"><strong><label id="totalTicket">'.$total.'</label>�</strong></td>' ;
-    $leticket .= '</tr></tbody>' ;
-    $leticket .= "</table>" ;
-    
-    // affichage dans la liste de gauche
-    $lesarticles .= "</table>" ;
-    echo $lesarticles ;              
-  ?>
-</div>
+        $flux->close();
 
-<div id="divSelection">
-  <div class="petitTitre">votre ticket</div><br />    
-  <div class="petitTexte"> 
+        // ajout d'une ligne s'il y a un tshirt 
+        if (isset($_SESSION["couleur"])) {
+            $nom = $_SESSION["couleur"];
+            $ficComplet = "images/tshirts/" . $nom . ".jpg";
+            // r�cup�ration des informations du tshirt
+            $titre = "t-shirt " . $nom;
+            $sujet = $prixTshirt;
+            $commentaire = "vous avez choisi un tshirt personnalis�.";
+            // construction de la ligne � gauche   
+            $lesarticles .= '<tbody id="ppanier' . $i . '"><tr id="trpanier' . $i . '">';
+            $lesarticles .= '<td class="td1"><img id="imgtshirt" src="' . $ficComplet . '" alt="' . $k . '" class="imgBoutique" /></td>';
+            $lesarticles .= '<td class="td2">' . $titre . '<br />' . $commentaire . '</td>';
+            $lesarticles .= '<td class="td3"><label>' . $sujet . '</label>�</td>';
+            $lesarticles .= '<td class="td4"><img id="supprtshirt' . $i . '" src="images/corbeille.jpg" alt="supprimer" /></td>';
+            $lesarticles .= '</tr></tbody>';
+            // construction de la ligne dans le ticket
+            $leticket .= '<tbody id="b' . $i . '"><tr id="trticket' . $i . '">';
+            $leticket .= '<td class="td2">' . $titre . '</td>';
+            $leticket .= '<td class="td3"><label id="prix' . $i . '">' . $sujet . '</label>�</td>';
+            $leticket .= '</tr></tbody>';
+            $total += $prixTshirt;
+        }
+
+        // calcul du total du ticket
+        $leticket .= '<tbody><tr id="trticket' . $i . '">';
+        $leticket .= '<td class="td2"><strong>TOTAL :</strong></td>';
+        $leticket .= '<td class="td3"><strong><label id="totalTicket">' . $total . '</label>�</strong></td>';
+        $leticket .= '</tr></tbody>';
+        $leticket .= "</table>";
+
+        // affichage dans la liste de gauche
+        $lesarticles .= "</table>";
+        echo $lesarticles;
+        ?>
+    </div>
+
+    <div id="divSelection">
+        <div class="petitTitre">votre ticket</div><br />    
+        <div class="petitTexte"> 
     <?php echo $leticket ?> 
-  </div>                                                  
-</div>
+        </div>                                                  
+    </div>
 
-<div id="divVisuel"></div>
+    <div id="divVisuel"></div>
 
-<?php 
+    <?php
 }
 
-include ("foot.php") ; ?>
+include ("foot.php");
+?>
--- perso.php
+++ perso.php
@@ -1,87 +1,94 @@
-<?php include ("head.php") ; ?>
+<?php include ("head.php"); ?>
 
-  <?php
-  // initialisation des variables 
-  $nom = "" ;
-  $prenom = "" ;
-  $adr1 = "" ;
-  $adr2 = "" ;
-  $cp = "" ;
-  $ville = "" ;
-  $infoslivraison = "" ;
-  $tel = "" ;
-  $mail = "" ;
-  $login = "" ;
-  $mdp = "" ;
-  $mdpverif = "" ;
+<?php
+// initialisation des variables 
+$nom = "";
+$prenom = "";
+$adr1 = "";
+$adr2 = "";
+$cp = "";
+$ville = "";
+$infoslivraison = "";
+$tel = "";
+$mail = "";
+$login = "";
+$mdp = "";
+$mdpverif = "";
 
-  // vérifie si la personne est connectée
-  if (isset($_SESSION["id"])) { 
-    // si elle est connectée, récupération de ses informations personnelles
-    $curseur = mysql_query("select * from client where numclient = ".$_SESSION["id"]) ;
-    $nom = mysql_result($curseur, 0, "nom") ;
-    $prenom = mysql_result($curseur, 0, "prenom") ;
-    $adr1 = mysql_result($curseur, 0, "adr1") ;
-    $adr2 = mysql_result($curseur, 0, "adr2") ;
-    $cp = mysql_result($curseur, 0, "cp") ;
-    $ville = mysql_result($curseur, 0, "ville") ;
-    $infoslivraison = mysql_result($curseur, 0, "infoslivraison") ;
-    $tel = mysql_result($curseur, 0, "tel") ;
-    $mail = mysql_result($curseur, 0, "mail") ;
-    $login = mysql_result($curseur, 0, "login") ;
-    $mdp = mysql_result($curseur, 0, "mdp") ;
-    $mdpverif = mysql_result($curseur, 0, "mdp") ;
-    $message = "Bienvenue dans votre espace personnel.<br />Vous pouvez consulter, modifier vos informations et les enregistrer." ;
-  }else{
-    // si c'est une nouvelle personne, message personnalisé (et les variables restent vides)
-    $message = "Bienvenue dans l'espace d'inscription.<br />Saisissez vos informations. Les zones avec * sont obligatoires." ;
-  }
-  
-  ?>
+// vï¿½rifie si la personne est connectï¿½e
+if (isset($_SESSION["id"])) {
+    // si elle est connectï¿½e, rï¿½cupï¿½ration de ses informations personnelles
 
-  <!-- zone d'explication -->
-  <div id="divExplication">
+    try {
+        $db = new PDO("mysql:host=localhost;dbname=boutique;charset=utf8", "root", "");
+        $mysecuredata = 14;
+        $curseur=$db->query("select * from client where numclient =" . $_SESSION["id"]);
+    } catch (PDOException $e) {
+        echo "Error: " . $e;
+    }
+
+    $nom = mysql_result($curseur, 0, "nom");
+    $prenom = mysql_result($curseur, 0, "prenom");
+    $adr1 = mysql_result($curseur, 0, "adr1");
+    $adr2 = mysql_result($curseur, 0, "adr2");
+    $cp = mysql_result($curseur, 0, "cp");
+    $ville = mysql_result($curseur, 0, "ville");
+    $infoslivraison = mysql_result($curseur, 0, "infoslivraison");
+    $tel = mysql_result($curseur, 0, "tel");
+    $mail = mysql_result($curseur, 0, "mail");
+    $login = mysql_result($curseur, 0, "login");
+    $mdp = mysql_result($curseur, 0, "mdp");
+    $mdpverif = mysql_result($curseur, 0, "mdp");
+    $message = "Bienvenue dans votre espace personnel.<br />Vous pouvez consulter, modifier vos informations et les enregistrer.";
+} else {
+    // si c'est une nouvelle personne, message personnalisï¿½ (et les variables restent vides)
+    $message = "Bienvenue dans l'espace d'inscription.<br />Saisissez vos informations. Les zones avec * sont obligatoires.";
+}
+?>
+
+<!-- zone d'explication -->
+<div id="divExplication">
     <div class="petitTitre">ESPACE PERSONNEL</div><br />
     <div class="petitTexte">
-      <?php echo $message ?>
+        <?php echo $message ?>
     </div>
-  </div>
+</div>
 
-  <!-- zone d'affichage des informations -->
-  <div id="divPerso" class="petitTexte">
-  <?php           
+<!-- zone d'affichage des informations -->
+<div id="divPerso" class="petitTexte">
+    <?php
     // affichage et saisie des informations de la personne
-    $k = 0 ;
-    $lesinfos = '<table id="tabPerso">' ;
-    $lesinfos .= uneCase("login", "login", $login, 20, true) ;    
-    $lesinfos .= uneCase("mdp", "mot de passe", $mdp, 20, true, true) ;    
-    $lesinfos .= uneCase("mdpverif", "mot de passe (contrôle)", $mdpverif, 20, true, true) ;    
-    $lesinfos .= uneCase("nom", "nom", $nom, 30, true) ;    
-    $lesinfos .= uneCase("prenom", "prenom", $prenom, 30, true) ;    
-    $lesinfos .= uneCase("adr1", "adresse", $adr1, 50, true) ;    
-    $lesinfos .= uneCase("adr2", "complément adresse", $adr2, 50) ;    
-    $lesinfos .= uneCase("cp", "code postal", $cp, 5, true) ;    
-    $lesinfos .= uneCase("ville", "ville", $ville, 30, true) ;    
-    $lesinfos .= uneCase("infoslivraison", "informations livraison", $infoslivraison, 50) ;    
-    $lesinfos .= uneCase("tel", "téléphone", $tel, 10) ;    
-    $lesinfos .= uneCase("mail", "adresse mail", $mail, 50) ;    
-    $lesinfos .= "</table>" ;
-    echo $lesinfos ;              
-  ?>
+    $k = 0;
+    $lesinfos = '<table id="tabPerso">';
+    $lesinfos .= uneCase("login", "login", $login, 20, true);
+    $lesinfos .= uneCase("mdp", "mot de passe", $mdp, 20, true, true);
+    $lesinfos .= uneCase("mdpverif", "mot de passe (contrï¿½le)", $mdpverif, 20, true, true);
+    $lesinfos .= uneCase("nom", "nom", $nom, 30, true);
+    $lesinfos .= uneCase("prenom", "prenom", $prenom, 30, true);
+    $lesinfos .= uneCase("adr1", "adresse", $adr1, 50, true);
+    $lesinfos .= uneCase("adr2", "complï¿½ment adresse", $adr2, 50);
+    $lesinfos .= uneCase("cp", "code postal", $cp, 5, true);
+    $lesinfos .= uneCase("ville", "ville", $ville, 30, true);
+    $lesinfos .= uneCase("infoslivraison", "informations livraison", $infoslivraison, 50);
+    $lesinfos .= uneCase("tel", "tï¿½lï¿½phone", $tel, 10);
+    $lesinfos .= uneCase("mail", "adresse mail", $mail, 50);
+    $lesinfos .= "</table>";
+    echo $lesinfos;
+    ?>
 </div>
 
 <!-- zone d'affichage des factures -->
 <div id="divSelection">
-  <div class="petitTitre">vos factures</div><br />    
-  <div class="petitTexte"> 
-    en construction<br />
-    l'historique de vos factures apparaitra ici 
-  </div>                                                  
+    <div class="petitTitre">vos factures</div><br />    
+    <div class="petitTexte"> 
+        en construction<br />
+        l'historique de vos factures apparaitra ici 
+    </div>                                                  
 </div>
 
 <!-- validation -->
 <div id="divEnregistrer">
-  <input id="cmdEnregistrer" type="button" value="Enregistrer" />
+    <input id="cmdEnregistrer" type="button" value="Enregistrer" />
 </div>
 
-<?php include ("foot.php") ; ?>
+<?php include ("foot.php"); ?>
