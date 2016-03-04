5009d3eccdbde5f8c2e706914e1505162a6ab281
MENTEAUX Kelian <MENTEAUX Kelian@CP-SIO-W10-PG13.sio.lan>
2016-03-04 09:21:52 +0100

    Problème mysql -> mysqli sur toutes les requêtes concernées fixé.

--- php/init.php
+++ php/init.php
@@ -13,20 +13,21 @@ $prixTshirt = 25 ;
 include_once ("chaines.php") ;
 include_once ("curseurs.php") ;
 include_once ("outils.php") ;
+$link = mysqli_connect("localhost", "root", "", "boutique");
 
 Connexion() ;
 
-// récupération de l'éventuel cookie
+// rï¿½cupï¿½ration de l'ï¿½ventuel cookie
 if (isset($_COOKIE["login"])) {
   $leId = ($_COOKIE["login"] + 27) / 353 ;
-  $curseur = mysql_query("select * from client where numclient=".$leId) ;
-  if (mysql_num_rows($curseur)!=0) {
+  $curseur = mysqli_query($link, "select * from client where numclient=".$leId) ;
+  if (mysqli_num_rows($curseur)!=0) {
     $_SESSION["login"] = mysql_result($curseur, 0, "login") ;
     $_SESSION["id"] = mysql_result($curseur, 0, "numclient") ;
   }
 }
 
-// récupération des variables de session éventuelles
+// rï¿½cupï¿½ration des variables de session ï¿½ventuelles
 if (isset($_SESSION["login"])) {
   $login = $_SESSION["login"] ;
   $id = $_SESSION["id"] ;  
