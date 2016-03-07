20f4fe8fe42b412869d31b8227e7fab92e608426
MENTEAUX Kelian <MENTEAUX Kelian@CP-SIO-W10-PG13.sio.lan>
2016-03-07 16:18:33 +0100

    Problème d'utf8 réglé. Problème des variables de sessions non fonctionnelles réglées. Problème de connexion non persistante réglée. Problème de page perso réglé.

--- perso.php
+++ perso.php
@@ -1,6 +1,9 @@
-<?php include ("head.php"); ?>
+<?php include ("head.php");
+$db = Connexion();
+?>
 
 <?php
+
 // initialisation des variables 
 $nom = "";
 $prenom = "";
@@ -15,25 +18,32 @@ $login = "";
 $mdp = "";
 $mdpverif = "";
 
+
+
 // v�rifie si la personne est connect�e
 if (isset($_SESSION["id"])) {
     // si elle est connect�e, r�cup�ration de ses informations personnelles
-
-     $curseur=Connexion()->query("select * from client where numclient =" . $_SESSION["id"]);
+   
+     $curseur = ("select * from client where numclient =" . $_SESSION["id"]);
+     $ligne = $db->prepare($curseur);
+     $ligne->execute();     
+     $unclient = $ligne->fetch(PDO::FETCH_OBJ);
+     
+     
  
 
-    $nom = mysql_result($curseur, 0, "nom");
-    $prenom = mysql_result($curseur, 0, "prenom");
-    $adr1 = mysql_result($curseur, 0, "adr1");
-    $adr2 = mysql_result($curseur, 0, "adr2");
-    $cp = mysql_result($curseur, 0, "cp");
-    $ville = mysql_result($curseur, 0, "ville");
-    $infoslivraison = mysql_result($curseur, 0, "infoslivraison");
-    $tel = mysql_result($curseur, 0, "tel");
-    $mail = mysql_result($curseur, 0, "mail");
-    $login = mysql_result($curseur, 0, "login");
-    $mdp = mysql_result($curseur, 0, "mdp");
-    $mdpverif = mysql_result($curseur, 0, "mdp");
+    $nom = utf8_encode($unclient->nom);
+    $prenom = utf8_encode($unclient->prenom);
+    $adr1 = utf8_encode($unclient->adr1);
+    $adr2 = utf8_encode($unclient->adr2);
+    $cp = utf8_encode($unclient->cp);
+    $ville = utf8_encode($unclient->ville);
+    $infoslivraison = utf8_encode($unclient->infoslivraison);
+    $tel = utf8_encode($unclient->tel);
+    $mail = utf8_encode($unclient->mail);
+    $login = utf8_encode($unclient->login);
+    $mdp = utf8_encode($unclient->mdp);
+    $mdpverif = utf8_encode($unclient->mdp);
     $message = "Bienvenue dans votre espace personnel.<br />Vous pouvez consulter, modifier vos informations et les enregistrer.";
 } else {
     // si c'est une nouvelle personne, message personnalis� (et les variables restent vides)
@@ -57,15 +67,15 @@ if (isset($_SESSION["id"])) {
     $lesinfos = '<table id="tabPerso">';
     $lesinfos .= uneCase("login", "login", $login, 20, true);
     $lesinfos .= uneCase("mdp", "mot de passe", $mdp, 20, true, true);
-    $lesinfos .= uneCase("mdpverif", "mot de passe (contr�le)", $mdpverif, 20, true, true);
+    $lesinfos .= uneCase("mdpverif", "mot de passe (contrôle)", $mdpverif, 20, true, true);
     $lesinfos .= uneCase("nom", "nom", $nom, 30, true);
     $lesinfos .= uneCase("prenom", "prenom", $prenom, 30, true);
     $lesinfos .= uneCase("adr1", "adresse", $adr1, 50, true);
-    $lesinfos .= uneCase("adr2", "compl�ment adresse", $adr2, 50);
+    $lesinfos .= uneCase("adr2", "complément adresse", $adr2, 50);
     $lesinfos .= uneCase("cp", "code postal", $cp, 5, true);
     $lesinfos .= uneCase("ville", "ville", $ville, 30, true);
     $lesinfos .= uneCase("infoslivraison", "informations livraison", $infoslivraison, 50);
-    $lesinfos .= uneCase("tel", "t�l�phone", $tel, 10);
+    $lesinfos .= uneCase("tel", "téléphone", $tel, 10);
     $lesinfos .= uneCase("mail", "adresse mail", $mail, 50);
     $lesinfos .= "</table>";
     echo $lesinfos;
